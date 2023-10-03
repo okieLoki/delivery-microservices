077019fab87df438a3428a95995e60b9354b649a
loki <uddeeptaraajkashyap@gmail.com>
2023-10-03 22:37:29 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, {Express} from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(8080, () => {
+  console.log('Server listening on port 3000');
+});
