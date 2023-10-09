2ac2c2afe3a9564b975c2fef124bb0529a44ad8b
loki <uddeeptaraajkashyap@gmail.com>
2023-10-09 23:43:11 +0530

    shop controller :feat:

---
+++ src/controllers/shoppingController.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from 'express'
+
+const getFoodAvailability = async (req: Request, res: Response) => {
+
+}
+
+const getTopResturants = async (req: Request, res: Response) => {
+
+}
+
+const getFoodin30Mins = async (req: Request, res: Response) => {
+
+}
+
+const searchFoods = async (req: Request, res: Response) => {
+
+}
+
+const resturantById = async (req: Request, res: Response) => {
+
+}
+
+export {
+    getFoodAvailability,
+    getTopResturants,
+    getFoodin30Mins,
+    searchFoods,
+    resturantById
+}
